7586cc3d61b6d6d95a3eb2f1f1d2bb1c33521dc1
pedrollovera <pllovera@grainchain.io>
2020-09-24 12:17:16 -0500

    Show time

--- gopath/src/gc_node_test_cc/contract/grain-contract.js
+++ gopath/src/gc_node_test_cc/contract/grain-contract.js
@@ -24,6 +24,7 @@ class grainContract extends Contract {
     console.log("Contract Found Succesfully..");
     return JSON.stringify(contract);
   }
+
 }
 
 module.exports.grainContract = grainContract;
--- gopath/src/gc_node_test_cc/index.js
+++ gopath/src/gc_node_test_cc/index.js
@@ -2,8 +2,10 @@
 
 const truckContract = require('./contract/truck-contract').truckContract;
 const grainContract = require('./contract/grain-contract').grainContract;
+const loadsContract = require('./contract/loads-contract').loadsContract;
 
 module.exports.grainContract = grainContract;
 module.exports.truckContract = truckContract;
+module.exports.loadsContract = loadsContract;
 
-module.exports.contracts = [truckContract, grainContract];
+module.exports.contracts = [truckContract, grainContract, loadsContract];
